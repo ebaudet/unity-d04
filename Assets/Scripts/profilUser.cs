50d542630d56888205ebe8c607e0ff86d4911177
Emilien Baudet <emilien.baudet@gmail.com>
2019-05-25 18:59:36 +0200

    DataSelect

--- Assets/Scripts/profilUser.cs
+++ Assets/Scripts/profilUser.cs
@@ -4,6 +4,125 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class profilUser : MonoBehaviour
+{
+	public int			rings = 0;
+	public int			lifeLosed = 0;
+	public List<Level>	levels = null;
+
+	public Text			countLifeLosed;
+	public Text			countRings;
+	
+	void Awake () {
+		if (PlayerPrefs.GetInt("rings") != 0)
+			rings = PlayerPrefs.GetInt("rings");
+		if (PlayerPrefs.GetInt("lifeLosed") != 0)
+			lifeLosed = PlayerPrefs.GetInt("lifeLosed");
+		if (PlayerPrefs.GetString("levels") != null) {
+			foreach (string level_string in PlayerPrefs.GetString("levels").Split(',')) {
+				if (level_string == "")
+					continue;
+				Debug.Log("coucou: "+level_string);
+				levels.Add(new Level(level_string));
+			}
+		}
+	}
+
+	private void Start() {
+		SetCountLifeLosed();
+		SetCountRings();
+	}
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			rings += 1;
+			SetCountRings();
+			SetRings();
+		}
+		if (Input.GetKeyDown(KeyCode.L)) {
+			lifeLosed += 1;
+			SetLifeLosed();
+			SetCountLifeLosed();
+		}
+		if (Input.GetKeyDown(KeyCode.Backspace)) {
+			Reset();
+			SetCountLifeLosed();
+			SetCountRings();
+		}
+	}
+
+	public void UpdateAll() {
+		SetRings();
+		SetLifeLosed();
+		SetLevels();
+	}
+	
+	public void Reset() {
+		rings = 0;
+		SetRings();
+		lifeLosed = 0;
+		SetLifeLosed();
+		levels = null;
+		SetLevels();
+		Save();
+	}
+
+	public void UpdateLevel(int levelId, int bestScore) {
+		addLevel(levelId, bestScore);
+		SetLevels();
+	}
+
+	public void SetLevels() {
+		string levelToStore = "";
+		int count = 0;
+		if (levels != null) {
+			foreach (Level level in levels) {
+				if (count > 0)
+					string.Concat(levelToStore, ",");
+				string.Concat(levelToStore, level.ToString());
+				count++;
+			}
+		}
+		PlayerPrefs.SetString("levels", levelToStore);
+	}
+
+	public void SetRings() {
+		PlayerPrefs.SetInt("rings", rings);
+	}
+
+	public void SetLifeLosed() {
+		PlayerPrefs.SetInt("lifeLosed", lifeLosed);
+	}
+
+	public void Save() {
+		PlayerPrefs.Save();
+	}
+
+	public void SetCountLifeLosed() {
+		countLifeLosed.text = lifeLosed.ToString();
+	}
+
+	public void SetCountRings() {
+		countRings.text = rings.ToString();
+	}
+
+	/***************** PRIVATE *****************/
+
+	private void OnDisable() {
+		Save();
+	}
+
+	private void addLevel(int levelId, int bestScore) {
+		foreach (Level level in levels) {
+			if (level.levelId == levelId) {
+				level.bestScore = bestScore;
+				return;
+			}
+		}
+		levels.Add(new Level(levelId, bestScore));
+	}
+}
+
 public class Level {
 	public int levelId;
 	public int bestScore;
@@ -14,70 +133,14 @@ public class Level {
 	}
 
 	public Level(string level_string) {
+		if (level_string == "")
+			return;
+		Debug.Log(level_string);
 		Int32.TryParse(level_string.Split('_')[0], out levelId);
 		Int32.TryParse(level_string.Split('_')[1], out bestScore);
 	}
 
 	override public string ToString() {
 		return levelId.ToString() + "_" + bestScore.ToString();
-	}
-}
-
-public class profilUser : MonoBehaviour
-{
-	public int			rings = 0;
-	public int			lifeLosed = 0;
-	public List<Level>	levels = null;
-	
-	void Awake () {
-		if (PlayerPrefs.GetInt("rings") != 0)
-			rings = PlayerPrefs.GetInt("rings");
-		if (PlayerPrefs.GetInt("lifeLosed") != 0)
-			lifeLosed = PlayerPrefs.GetInt("lifeLosed");
-		if (PlayerPrefs.GetString("levels") != null) {
-			foreach (string level in PlayerPrefs.GetString("levels").Split(',')) {
-				levels.Add(new Level(level));
-			}
-		}
-	}
-
-	private void OnDisable() {
-		Save();
-	}
-
-	public void UpdateLevel(int levelId, int bestScore) {
-		addLevel(levelId, bestScore);
-		string levelToStore = "";
-		int count = 0;
-		foreach (Level level in levels) {
-			if (count > 0)
-				string.Concat(levelToStore, ",");
-			string.Concat(levelToStore, level.ToString());
-			count++;
-		}
-		PlayerPrefs.SetString("levels", levelToStore);
-	}
-
-	public void UpdateRings(int ring) {
-		PlayerPrefs.SetInt("rings", ring);
-	}
-
-	public void UpdateLifeLosed(int lifeLosed) {
-		PlayerPrefs.SetInt("lifeLosed", lifeLosed);
-	}
-
-	public void Save() {
-		PlayerPrefs.Save();
-	}
-
-	/***************** PRIVATE *****************/
-	private void addLevel(int levelId, int bestScore) {
-		foreach (Level level in levels) {
-			if (level.levelId == levelId) {
-				level.bestScore = bestScore;
-				return;
-			}
-		}
-		levels.Add(new Level(levelId, bestScore));
 	}
 }
